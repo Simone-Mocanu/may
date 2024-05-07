23f87b0903f616e89ea04e48c86983e4e09300c6
beji <simone0443@outlook.com>
2024-05-07 07:21:36 +0300

    final commit

--- may.py
+++ may.py
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import time
+desired_message = "IT'S FINALLY MAY, SO MAY I BE YOURS?"
+
+chars = "'?,abcdefghijklmnopqrstuvwxyz "
+
+message = ""
+for i in desired_message: 
+    if(i == " "):
+        message += i
+        continue
+    for j in chars:
+        if(j == " "):
+            message += i
+
+        print(j.upper() + message)
+        time.sleep(0.04)
